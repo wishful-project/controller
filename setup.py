2d8c1092e387a39b9f2d38d7679980125fe5a183
Mikolaj Chwalisz <chwalisz@tkn.tu-berlin.de>
2015-11-11 10:14:10 +0100

    small fixes in setup

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from distutils.core import setup
 
 setup(
     name='wishful_upis',
-    version='0.1',
+    version='0.1.0',
     packages=['wishful_upis'],
     url='http://www.wishful-project.eu/software',
     license='',
     author='Mikolaj Chwalisz',
     author_email='chwalisz@tkn.tu-berlin.de',
-    description='Implementation of a wireless controller using the unified programming interfaces (UPIs) of the Wishful software platform for radio and network control.'
+    description='Implementation of a wireless controller using the unified programming interfaces (UPIs) of the Wishful project.',
+    keywords='wireless control',
+    install_requires=['pyzmq']
 )
