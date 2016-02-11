8c4d90cff68aa2813b1e5aed74951254e4dcab87
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-02-11 13:55:40 +0100

    create functions based on wishful_upis

--- wishful_controller/upis/upis.py
+++ wishful_controller/upis/upis.py
@@ -1,5 +1,8 @@
 import logging
+import inspect
 import wishful_upis
+import imp
+import decorator
 
 __author__ = "Piotr Gawlowicz, Mikolaj Chwalisz"
 __copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
@@ -8,16 +11,66 @@ __email__ = "{gawlowicz, chwalisz}@tkn.tu-berlin.de"
 
 #http://code.activestate.com/recipes/577659-decorators-for-adding-aliases-to-methods-in-a-clas/
 
+@decorator.decorator
+def _add_function(fn, *args, **kwargs):
+    def wrapped(self, *args, **kwargs):
+        print dir(self)
+        print self.__dict__
+        print args
+        print kwargs
+        print self._ctrl
+        print self._msg_type
+        return wrapped
+    return wrapped(*args, **kwargs)
+
+class UpiBase(object):
+    def __init__(self):
+        self._ctrl = None
+        self._msg_type = None
+
+class UpiRadio(UpiBase):
+    def __init__(self, ctrl):
+        self._ctrl = ctrl
+        self._msg_type = "radio"
+
+class UpiNet(UpiBase):
+    def __init__(self, ctrl):
+        self._ctrl = ctrl
+        self._msg_type = "net"
+
+class UpiMgmt(UpiBase):
+    def __init__(self, ctrl):
+        self._ctrl = ctrl
+        self._msg_type = "mgmt"
+
+
 class UpiBuilder(object):
     def __init__(self, ctrl):
         self._ctrl = ctrl
 
     def create_radio(self):
+        
         for method in dir(wishful_upis.radio):
             if callable(getattr(wishful_upis.radio, method)):
-                print method
                 function = getattr(wishful_upis.radio, method)
-                setattr(UpiRadio, method, staticmethod(function))
+
+                # Based on:
+                # http://stackoverflow.com/questions/10303248/true-dynamic-and-anonymous-functions-possible-in-python
+                # TODO: is it possible to make it better? change signature?
+                # https://www.python.org/dev/peps/pep-0362/#visualizing-callable-objects-signature
+                
+                fargs = inspect.getargspec(function).args
+                fargs_str = ",".join(fargs)
+                code = """def {}(self,{}): pass
+                """.format(method,fargs_str)
+
+                module = imp.new_module('myfunctions')
+                exec code in module.__dict__
+                function = getattr(module, method)
+                del module
+
+                decorated_fun = _add_function(function)
+                setattr(UpiRadio, method, decorated_fun)
         radio = UpiRadio(self._ctrl)
         return radio
 
@@ -26,18 +79,15 @@ class UpiBuilder(object):
             if callable(getattr(wishful_upis.net, method)):
                 print method
                 function = getattr(wishful_upis.net, method)
-                setattr(UpiRadio, method, staticmethod(function))
+                setattr(UpiRadio, method, classmethod(function))
         net = UpiNet(self._ctrl)
         return net
 
 
-class UpiRadio(object):
-    def __init__(self, ctrl):
-        self._ctrl = ctrl
-        self._msg_type = "radio"
 
-
-class UpiNet(object):
-    def __init__(self, ctrl):
-        self._ctrl = ctrl
-        self._msg_type = "net"
+if __name__ == "__main__":
+    builder = UpiBuilder(1)
+    radio = builder.create_radio()
+    print(inspect.getargspec(radio.set_channel))
+    print radio.set_channel
+    radio.set_channel(channel=1)
