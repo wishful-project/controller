36440dde97176d53d7affb51c4517ecdf46d1f29
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-02-29 22:59:17 +0100

    controller module updated

--- wishful_controller/controller_module.py
+++ wishful_controller/controller_module.py
@@ -1,59 +1,72 @@
 import logging
-import subprocess
-import zmq.green as zmq
+import zmq
 import random
+import sys
+import time
+import threading
+import wishful_framework as msgs
+try:
+   import cPickle as pickle
+except:
+   import pickle
 
 __author__ = "Piotr Gawlowicz, Mikolaj Chwalisz"
-__copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
+__copyright__ = "Copyright (c) 2015, Technische Universität Berlin"
 __version__ = "0.1.0"
 __email__ = "{gawlowicz, chwalisz}@tkn.tu-berlin.de"
 
-class ControllerModule(object):
-    def __init__(self, name, path, args):
-        self.log = logging.getLogger("{module}.{name}".format(
-            module=self.__class__.__module__, name=self.__class__.__name__))
-        self.name = name
-        self.path = path
-        self.args = args
-        self.port = None
 
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PAIR)
+class discover_controller(object):
+    def __init__(self, ):
+        self.discover_controller = True
 
-        self.start_server_for_module()
-        self.start_module_process()
-        pass
+    def __call__(self, f):
+        f._discover_controller = self.discover_controller
+        return f
 
-    def start_server_for_module(self):
+class loop(object):
+    def __init__(self, ):
+        self.loop = True
 
-        self.port = random.randint(5000, 10000)
-        while True:
-            try:
-                self.socket.bind("tcp://*:%s" % self.port)
-                break
-            except:
-                self.port = random.randint(5000, 10000)
-
-        self.log.debug("Server for {} started on port: {} ".format(self.name, self.port))
-
-    def start_module_process(self):
-        cmd = [self.path,
-               '--port', str(self.port)
-               ]
-        cmd.extend(filter(None, self.args))
-        self.pid = subprocess.Popen(cmd)
-        self.log.debug("Module: {}, with args: {}, PID: {} started".format(self.name, self.args, self.pid.pid))
-
-    def exit(self):
-        self.pid.kill()
-        pass
+    def __call__(self, f):
+        f._loop = self.loop
+        return f
 
 
-    def send_msg_to_module(self, msgContainer):
-        self.log.debug("Module: {} sends msg".format(self.name))
-        self.socket.send_multipart(msgContainer)
+class on_start(object):
+    def __init__(self, ):
+        self.onStart = True
+
+    def __call__(self, f):
+        f._onStart = self.onStart
+        return f
 
 
+class on_exit(object):
+    def __init__(self):
+        self.onExit = True
+
+    def __call__(self, f):
+        f._onExit = self.onExit
+        return f
+
+
+class on_connected(object):
+    def __init__(self):
+        self.onConnected = True
+
+    def __call__(self, f):
+        f._onConnected = self.onConnected
+        return f
+
+
+class on_disconnected(object):
+    def __init__(self):
+        self.onDisconnected = True
+
+    def __call__(self, f):
+        f._onDisconnected = self.onDisconnected
+        return f
 
 
 class bind_function(object):
@@ -65,19 +78,85 @@ class bind_function(object):
         f._upi_fname = self.upi_fname
         return f
 
-
 def build_module(module_class):
     original_methods = module_class.__dict__.copy()
     for name, method in original_methods.iteritems():
         if hasattr(method, '_upi_fname'):
-            #create alias
+            #add UPI alias for the function
             for falias in method._upi_fname - set(original_methods):
                 setattr(module_class, falias, method)
     return module_class
 
 
-class ControllerUpiModule(object):
-    def __init__(self, controller):
+class ControllerModule(object):
+    def __init__(self):
         self.log = logging.getLogger("{module}.{name}".format(
             module=self.__class__.__module__, name=self.__class__.__name__))
-        self.controller = controller
+
+        self.id = None
+        self.name = self.__class__.__name__
+        self.capabilities = []
+
+        #discover UPI function implementation and create upi_capabilities list
+        func_name = [method for method in dir(self) if callable(getattr(self, method)) and hasattr(getattr(self, method), '_upi_fname')]
+        self.upi_callbacks = {list(getattr(self, method)._upi_fname)[0] : method for method in func_name}
+        self.upis_capabilities = self.upi_callbacks.keys()
+        
+        #interface to be used in UPI functions, it is set before function call
+        self.interface = None
+
+
+    def get_capabilities(self):
+        return self.upis_capabilities
+
+
+    def send_to_module(self, msgContainer):
+        self.log.debug("Module {} received cmd".format(self.__class__.__name__))
+        pass
+
+
+    def set_controller(self):
+        #discover controller discovery function
+        pass
+
+
+    def execute_function(self, func):
+        loop = hasattr(func, '_loop')
+        if loop:
+            self.threads = threading.Thread(target=func)
+            self.threads.setDaemon(True)
+            self.threads.start()
+        else:
+            func()
+
+
+    def start(self):
+        #discover all functions that have to be executen on start
+        funcs = [method for method in dir(self) if callable(getattr(self, method)) and hasattr(getattr(self, method), '_onStart')]
+        for fname in funcs:
+            f = getattr(self, fname)
+            self.execute_function(f)
+
+
+    def exit(self):
+        #discover all functions that have to be executen on exit
+        funcs = [method for method in dir(self) if callable(getattr(self, method)) and hasattr(getattr(self, method), '_onExit')]
+        for fname in funcs:
+            f = getattr(self, fname)
+            self.execute_function(f)
+
+
+    def connected(self):
+        #discover all functions that have to be executen on connected
+        funcs = [method for method in dir(self) if callable(getattr(self, method)) and hasattr(getattr(self, method), '_onConnected')]
+        for fname in funcs:
+            f = getattr(self, fname)
+            self.execute_function(f)
+
+
+    def disconnected(self):
+        #discover all functions that have to be executen on disconnected
+        funcs = [method for method in dir(self) if callable(getattr(self, method)) and hasattr(getattr(self, method), '_onDisconnected')]
+        for fname in funcs:
+            f = getattr(self, fname)
+            self.execute_function(f)
